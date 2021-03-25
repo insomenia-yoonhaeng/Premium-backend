97a8b0bb53ae6001287ec84c5bbe5bc658f254fb
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-03-25 21:32:01 +0900

    [#9] tdd test 1st complete

--- Gemfile
+++ Gemfile
@@ -44,8 +44,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'faker'
-  gem 'factory_girl'
-  gem 'factory_bot_rails'
+  gem 'factory_girl_rails'
   gem 'database_cleaner'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    User.create(email: "testuser@test.com", password: "password", name: "홍길동", phone: "010-1234-5678")
+  end
+
+  context "유저 생성 관련" do
+    it "잘못된 이메일 입력" do
+      user = User.create(email: nil, password: "password", name: Faker::Name::name.gsub(/\s+/, ""))
+      expect(user).to validate_presence_of(:email)
+    end
+    xit "중복 이메일 입력" do
+      user = User.create(email: "testuser@test.com", password: "password", name: Faker::Name::name.gsub(/\s+/, ""))
+      expect(user).to eq nil
+    end
+    xit "잘못된 이름 입력" do
+      user = User.create(email: nil, password: "password", name: Faker::Name::name.gsub(/\s+/, ""))
+      expect(user).to eq nil
+    end
+  end
 end
