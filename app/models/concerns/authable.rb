527e3e3a41fae85b269c596e926643a0028f7aaf
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-03-26 23:38:52 +0900

    [#13] fix bug

--- app/models/concerns/authable.rb
+++ app/models/concerns/authable.rb
@@ -1,7 +1,7 @@
 module Authable
   extend ActiveSupport::Concern
   included do
-    has_many :auths, as: :target, class_name: 'Auth', dependent: :destroy # 인증이 많을 수 있다. 학생의 경우 챕터마다 인증
+    has_many :auths, as: :authable, class_name: 'Auth', dependent: :destroy # 인증이 많을 수 있다. 학생의 경우 챕터마다 인증
     
 		# 인증을 받았는지 확인
     def is_authed? _user
