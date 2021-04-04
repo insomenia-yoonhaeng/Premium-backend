b8b37cc129b8cfdd8301f7da5f13805aab6172ab
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-04-04 17:33:11 +0900

    hot fix db

--- db/migrate/20210401123652_add_experience_period_to_project.rb
+++ db/migrate/20210401123652_add_experience_period_to_project.rb
@@ -1,5 +1,6 @@
 class AddExperiencePeriodToProject < ActiveRecord::Migration[6.0]
   def change
+    remove_column :projects, :experience_period
     add_column :projects, :experience_period, :integer, default: 0
   end
 end
