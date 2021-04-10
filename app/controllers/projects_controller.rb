15acb559608d6e56f21ddac09027b82ec6bc373b
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-04-10 21:30:10 +0900

    [#10] refactor project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,57 +1,61 @@
 class ProjectsController < ApiController
 	before_action :authorize_request
-	before_action :check_user_type, except: %i(index show)
   before_action :load_project, except: %i(index create)
 	
 	def index
-		projects = Project.ransack(params[:q]).result
-		render json: each_serializer(projects, ProjectSerializer)
+		begin
+			projects = Project.ransack(params[:q]).result
+			render json: each_serializer(projects, ProjectSerializer)
+		rescue => exception
+			render json: { error: projects&.errors&.full_messages&.first }, status: :bad_request
+		end
 	end
 
 	def create
 		begin
-			@project = @current_user.projects.create(project_params)
-			render json: serializer(@project, ProjectSerializer) 
+			project = current_user&.projects&.create(project_params)
+			render json: serializer(project, ProjectSerializer) 
 		rescue => exception
-			render json: {error: @project&.errors&.full_messages&.first}, status: :bad_request
+			render json: {error: project&.errors&.full_messages&.first}, status: :bad_request
 		end
 	end
 	
-	# 일단은 처음이라 튜터도 플젝 여러개, 튜티도 플젝 여러개이므로
 	def show
+		# show는 자신의 프로젝트가 아니더라도 접근 가능해야하므로 load_proejct로 참조해오는 것은 적합하지 않음, 체험기간에도 볼 수 있어야하므로
 		begin
-			render json: serializer(@project, ProjectSerializer)
+			proejct = Proejct.find(params[:id])
+			render json: serializer(project, ProjectSerializer)
 		rescue => exception
-			render json: {error: @project.errors&.full_messages&.first}, status: :bad_request
+			render json: {error: project.errors&.full_messages&.first}, status: :bad_request
 		end
 	end
 
 	def update
 		begin
-			@project.update(project_params)
-			render json: serializer(@project, ProjectSerializer)
+			project.update(project_params)
+			render json: serializer(project, ProjectSerializer)
 		rescue => exception
-			render json: {error: @project&.errors&.full_messages&.first}, status: :bad_request			
+			render json: {error: project&.errors&.full_messages&.first}, status: :bad_request			
 		end
 	end
 
 	def destroy
 		begin
-			@project.destroy
+			project.destroy
 			render json: { status: :ok }
 		rescue => exception
-			render json: {error: @project&.errors&.full_messages&.first}, status: :bad_request			
+			render json: {error: project&.errors&.full_messages&.first}, status: :bad_request			
 		end
 	end
 	
 	protected
 
 	def load_project
-		@project = @current_user.projects.find(params[:id]) if @current_user.project.present?
+		project = current_user.projects.find(params[:id]) if current_user.project.present?
 	end
 
 	def project_params
-	 params.require(:project).permit(Project::PERMIT_COLUMNS)
+		params.require(:project).permit(Project::PERMIT_COLUMNS)
 	end
 
 end
