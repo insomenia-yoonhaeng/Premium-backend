a05801ab1d0210a0fa81a60f90b6c5d434f95820
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-04-11 08:23:53 +0900

    [#10] exception handling book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,9 +22,15 @@ class BooksController < ApiController
 
   def get_list # 목차 가져오기
     # https://sampatbadhe.medium.com/rake-task-invoke-or-execute-419cd689c3bd
-    path = "https://search.daum.net/search?w=bookpage&bookId=848781&q=%EC%A4%91%ED%95%99+%EC%88%98%ED%95%99+2-1%282021%29%28%EA%B0%9C%EB%85%90%EC%9B%90%EB%A6%AC%29"
-    Rake::Task['crawl:example'].execute(url: path, book: Book.find(11))
-    render json: serializer(Book.find(11), BookSerializer), status: :ok
+    if @book ||= Book.find_by(title: params.dig(:book, :title))
+      puts @book
+      render json: serializer(@book, BookSerializer), status: :ok 
+    else
+      path = "https://search.daum.net/search?w=bookpage&bookId=848781&q=%EC%A4%91%ED%95%99+%EC%88%98%ED%95%99+2-1%282021%29%28%EA%B0%9C%EB%85%90%EC%9B%90%EB%A6%AC%29"
+      # parameter 감쌀 때 book: {title: "fdsafdsfads", url: "dfdsafdasfdas"} 으로 요청하기
+      Rake::Task['crawl:example'].execute(url: path, book: Book.find(11))
+      render json: serializer(Book.find(11), BookSerializer), status: :ok
+    end
   end
 
   private 
