1b49d1e9405944c107fa327ef199eca33e87a586
Itsbeenalongday <dbtjdals1771@ajou.ac.kr>
2021-04-29 19:29:20 +0900

    [#10] merge with develop add url to book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_143929) do
+ActiveRecord::Schema.define(version: 2021_04_29_102450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_04_11_143929) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -73,6 +74,15 @@ ActiveRecord::Schema.define(version: 2021_04_11_143929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_chapters_on_book_id"
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "images", force: :cascade do |t|
@@ -134,7 +144,6 @@ ActiveRecord::Schema.define(version: 2021_04_11_143929) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password_digest"
     t.string "phone"
     t.string "name", null: false
     t.text "info"
@@ -144,11 +153,23 @@ ActiveRecord::Schema.define(version: 2021_04_11_143929) do
     t.string "image"
     t.string "type"
     t.integer "likes_count", default: 0
+    t.string "account_type"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone"], name: "index_users_on_phone", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "chapters", "books"
+  add_foreign_key "identities", "users"
   add_foreign_key "likes", "users"
   add_foreign_key "options", "chapters"
   add_foreign_key "options", "users", column: "tutor_id"
