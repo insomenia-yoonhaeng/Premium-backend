e47d6b6a27412934d5f222266e2ec86086b66c34
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-05-02 15:07:36 +0900

    hot fix

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < Panko::Serializer
-  attributes :deposit, :description, :image, :experience_period, :title
+  attributes :id, :deposit, :description, :image, :experience_period, :title
 
 	#belongs_to :tutor, serializer: UserSerializer
 	#has_many :tutees, each_serializer: UserSerializer
