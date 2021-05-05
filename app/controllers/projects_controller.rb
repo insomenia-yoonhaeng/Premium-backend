4edee7206a7270b946d6e9f9f1ca597853786dff
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-05-05 22:39:36 +0900

    [#34] merge

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,11 +26,7 @@ class ProjectsController < ApiController
 	def show
 		# show는 자신의 프로젝트가 아니더라도 접근 가능해야하므로 load_project로 참조해오는 것은 적합하지 않음, 체험기간에도 볼 수 있어야하므로
 		begin
-<<<<<<< Updated upstream
-			proejct = Proejct.find(params[:id])
-=======
 			project = Project.find(params[:id])
->>>>>>> Stashed changes
 			render json: serializer(project, ProjectSerializer)
 		rescue => exception
 			render json: {error: project.errors&.full_messages&.first}, status: :bad_request
