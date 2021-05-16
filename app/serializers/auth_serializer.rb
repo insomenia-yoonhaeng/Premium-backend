63055893ef5d6751ef5a60e9eb8b0f08948c82eb
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-05-16 15:09:19 +0900

    fix authserializer

--- app/serializers/auth_serializer.rb
+++ app/serializers/auth_serializer.rb
@@ -1,11 +1,8 @@
 class AuthSerializer < Panko::Serializer
-  attributes :id, :description, :authable_type, :authable_id, :created_at, :tutee
+  attributes :id, :description, :authable_type, :authable_id, :created_at
 	
 	#belongs_to :tutor, serializer: UserSerializer
 	has_one :tutee, serializer: UserSerializer
 	has_many :images, each_serializer: ImageSerializer
-
-	def tutee
-		context[:tutee] if context[:tutee].present?
-	end
+	
 end
