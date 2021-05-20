7bd481e35cfe75720f35fe38104ef7c9367f6ae1
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-05-20 20:05:47 +0900

    fix admin title

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < Panko::Serializer
-  attributes :description, :deposit ,:image ,:title ,:started_at ,:duration ,:experience_period ,:category_id ,:required_time ,:review_weight ,:mission ,:book_id ,:rest
+  attributes :id, :description, :deposit ,:image ,:title ,:started_at ,:duration ,:experience_period ,:category_id ,:required_time ,:review_weight ,:mission ,:book_id ,:rest
   
 	has_one :tutor, serializer: UserSerializer
 end
