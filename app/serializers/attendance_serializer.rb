7449252b3c8f506879607038eb89c21d1be670cd
Yoon-Haeng-Heo <submailid96@gmail.com>
2021-05-22 14:09:42 +0900

    fix query

--- app/serializers/attendance_serializer.rb
+++ app/serializers/attendance_serializer.rb
@@ -1,7 +1,11 @@
 class AttendanceSerializer < Panko::Serializer
 
-  attributes :id, :status, :created_at, :updated_at, :pay_status
+  attributes :id, :status, :created_at, :updated_at, :pay_status, :attendance_count
 
   has_one :project, each_serializer: ProjectSerializer
   has_one :tutee, each_serializer: TuteeSerializer
+
+  def attendance_count
+    Project.includes(:attendances).find(object.project.id).attendances.count if object.project.present?
+  end
 end
